31991ecb50e4dfd961c0e9cdd81a1661aa450366
Tumon <tumon@debian.Tumon>
2022-05-28 03:29:23 +0600

    init

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -111,12 +111,12 @@ const ProductDetails = ({ product, products }) => {
 
                         {/* ==== ADD AND BUY */}
                         <div className=' sm:flex w-full gap-4 mt-4 px-1'>
-                            <button className=' text-center mb-4 sm:mb-0 hover:scale-105 transition
+                            <div className=' text-center mb-4 sm:mb-0 hover:scale-105 transition
                              text-xl px-8 py-2 ring-1 ring-red-600 w-full '
                                 onClick={() => onAdd(product, qty)}
                             >
                                 Add to Cart
-                            </button>
+                            </div>
 
                             <div className=' text-center hover:scale-105 transition shadow-md
                             bg-red-600 text-xl px-8 py-2  text-white ring-1 ring-red-600'>
