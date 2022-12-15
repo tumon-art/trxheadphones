b3fe1876a5b374566f3fbb99f284cce6937009fe
tumon <tumonkhn@gmail.com>
2022-12-16 03:02:50 +0600

    .

--- comps/Products.tsx
+++ comps/Products.tsx
@@ -49,13 +49,46 @@ const Products = ({ products, gap }: ProductsProps) => {
      transition my-5 `}
     >
       <div
-        className="cursor-pointer shadow-sm shadow-lightDim overflow-hidden
-            rounded-md  bg-lightDim h-32 w-32 sm:h-40 sm:w-40 lg:h-56 lg:w-56"
+        className=" relative cursor-pointer shadow-sm shadow-lightDim overflow-hidden
+            rounded-md h-32 w-32 sm:h-40 sm:w-40 lg:h-56 lg:w-56"
       >
         {/* === IMAGE */}
         <Link href={`/product/${products.slug.current}`}>
           <Img className="object-cover" alt="headphone" {...imageProps} />
         </Link>
+
+        {/* === FAV ICON */}
+        {isLoaded && (
+          <svg
+            onClick={() => {
+              saveToLocalS(products);
+              setUpdate((p) => !p);
+            }}
+            className={`h-6 top-0 right-0 stroke-lightGray  sm:h-8 absolute hover:stroke-love self-start 
+          sm:hover:fill-love transition-colors
+          duration-1000 text-lightDim1 z-10 ${
+            window.localStorage.trxfav &&
+            JSON.parse(localStorage.trxfav).filter(
+              (each: ProductsTypes) => each._id == products._id
+            ).length >= 1 &&
+            "fill-love stroke-love"
+          }`}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="none"
+            strokeWidth={2}
+          >
+            <title> Add To Favorite</title>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5
+          4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            />
+          </svg>
+        )}
+
+        <div className=" absolute h-2/4 w-full bg-lightDim1 bottom-0 z-[-2]"></div>
       </div>
 
       {/* === NAME & PRICE */}
@@ -70,37 +103,6 @@ const Products = ({ products, gap }: ProductsProps) => {
             <b className=" text-zinc-900 "> ${products.price} </b>
           </div>
         </nav>
-
-        {/* === FAV ICON */}
-        {isLoaded && (
-          <svg
-            onClick={() => {
-              saveToLocalS(products);
-              setUpdate((p) => !p);
-            }}
-            className={`h-6 sm:h-10 self-start hover:bg-lightLove
-          sm:hover:fill-love transition-colors
-          duration-1000 text-lightDim1 z-10 ${
-            window.localStorage.fav &&
-            JSON.parse(localStorage.trxfav).filter(
-              (each: ProductsTypes) => each._id == products._id
-            ).length >= 1 &&
-            "fill-love"
-          }`}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <title> Add To Favorite</title>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5
-          4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-            />
-          </svg>
-        )}
       </section>
     </div>
   );
